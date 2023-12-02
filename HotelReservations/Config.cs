9926496d09ca5cd9f8c121e9d10b1a11887a38c7
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-12-02 10:30:04 +0100

    build App Structure and init Db Connection

---
+++ HotelReservations/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservations
+{  
+    //Db Connection
+    public static class Config
+    {
+        public static string CONNECTION_STRING { get; } = "Server=DESKTOP-H9NQTJ3;Database=HotelReservations;Trusted_Connection=True;TrustServerCertificate=True";
+    }
+}
