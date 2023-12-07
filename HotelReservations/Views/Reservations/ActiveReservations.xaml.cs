b840fffee58b469a6ea37ca91aa9e8ab2c1dd757
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-12-07 12:15:17 +0100

    Active and AddEdit Reservation are done

--- HotelReservations/Views/Reservations/ActiveReservations.xaml.cs
+++ HotelReservations/Views/Reservations/ActiveReservations.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using HotelReservations.Model;
+using HotelReservations.Service;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,41 @@ namespace HotelReservations.Views.Reservations
     /// </summary>
     public partial class ActiveReservations : Window
     {
+        private ICollectionView view;
         public ActiveReservations()
         {
             InitializeComponent();
+            FillData();
         }
+        public void FillData()
+        {
+            var reservationService = new ReservationService();
+            var reservations = Hotel.GetInstance().Reservations.Where(res => res.reservation_is_active).ToList();
+
+            ReservationsDataGrid.ItemsSource = null; // Clear existing items
+
+            foreach (var reservation in reservations)
+            {
+                ReservationsDataGrid.Items.Add(reservation);
+
+            }
+
+            ReservationsDataGrid.IsSynchronizedWithCurrentItem = true;
+            ReservationsDataGrid.SelectedItem = null;
+        }
+
+        private void GuestsDataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName == "Id")
+            {
+                e.Column.Visibility = Visibility.Collapsed;
+            }
+            if (e.PropertyName == "guest_reservation_id")
+            {
+                e.Column.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
     }
 }
--- HotelReservations/Views/Reservations/AddEditReservations.xaml.cs
+++ HotelReservations/Views/Reservations/AddEditReservations.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using HotelReservations.Model;
+using HotelReservations.Service;
+using HotelReservations.Views.Reservations.Guests;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +23,268 @@ namespace HotelReservations.Views.Reservations
     /// </summary>
     public partial class AddEditReservations : Window
     {
-        public AddEditReservations()
+        private ReservationService reservationService;
+        private Reservation contextReservation;
+        private ICollectionView view;
+        public bool isEditing;
+
+
+        // this will be for creating reservation by picking room
+        public AddEditReservations(Reservation? res = null)
         {
             InitializeComponent();
+            if (res == null)
+            {
+                contextReservation = new Reservation();
+                isEditing = false;
+                AddGuestButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                contextReservation = res.Clone();
+                isEditing = true;
+                AddGuestButton.Visibility = Visibility.Hidden;
+            }
+
+            reservationService = new ReservationService();
+            AdjustWindow(res);
+            FillData(res);
+            this.DataContext = contextReservation;
         }
+
+        private bool DoFilter(object roomObject)
+        {
+            var room = roomObject as Room;
+            var reservations = Hotel.GetInstance().Reservations.Where(res => res.reservation_room_number == room.room_number);
+
+            var roomTypeValue = RoomTypeComboBox.SelectedValue;
+            DateTime? startDate = CheckStartDate.SelectedDate;
+            DateTime? endDate = CheckEndDate.SelectedDate;
+
+            /* you might be a little bit confused here so i will explain short
+            im checking is room type even selected so if its not selected i will just return true */
+
+            if (roomTypeValue != null && roomTypeValue != "")
+            {
+                // if its selected then we will compare and also we will iterate over loop to check are dates overlapping(means available dates if not overlap)
+                bool roomType = room.RoomType.ToString() == roomTypeValue.ToString();
+
+                if (roomType == false)
+                {
+                    return false;
+                }
+
+                foreach (Reservation r in reservations)
+                {
+                    if (AreDatesOverlapping(startDate, endDate, r.start_date_time, r.end_date_time))
+                    {
+                        return false; // If there is an overlap, return false
+                    }
+                }
+
+            }
+
+            return true;
+        }
+
+        // this is just helper function to check are dates overlapping
+        private bool AreDatesOverlapping(DateTime? start1, DateTime? end1, DateTime? start2, DateTime? end2)
+        {
+            if (!start1.HasValue || !end1.HasValue || !start2.HasValue || !end2.HasValue)
+            {
+                return false; // if something is null, its no overlapped
+            }
+
+            if (start1 >= start2 && start1 <= end2)
+            {
+                return true;
+            }
+            if (end1 >= start2 && end1 <= end2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void AdjustWindow(Reservation? res = null)
+        {
+            var roomTypeList = Hotel.GetInstance().RoomTypes.Where(roomType => roomType.room_type_is_active).ToList();
+            RoomTypeComboBox.ItemsSource = roomTypeList;
+
+            if (res != null)
+            {
+                Title = "Edit Reservation";
+            }
+            else
+            {
+                Title = "Add Reservation";
+            }
+        }
+
+        public void FillData(Reservation? res = null, Guest? newGuest = null)
+        {
+            var guests = Hotel.GetInstance().Guests.Where(g => g.guest_is_active && g.guest_reservation_id == 0);
+
+            if (isEditing)
+            {
+                guests = Hotel.GetInstance().Guests?.Where(g => g.guest_reservation_id == contextReservation.reservation_id && g.guest_is_active) ?? Enumerable.Empty<Guest>();
+            }
+
+            if (newGuest != null)
+            {
+                guests.Prepend(newGuest);
+            }
+
+            var rooms = Hotel.GetInstance().Rooms.Where(room => room.room_is_active).ToList();
+
+            view = CollectionViewSource.GetDefaultView(guests);
+            GuestsDataGrid.ItemsSource = null;
+            GuestsDataGrid.ItemsSource = view;
+            GuestsDataGrid.IsSynchronizedWithCurrentItem = true;
+            GuestsDataGrid.SelectedItem = null;
+
+            view = CollectionViewSource.GetDefaultView(rooms);
+            view.Filter = DoFilter;
+            RoomsDataGrid.ItemsSource = null;
+            RoomsDataGrid.ItemsSource = view;
+            RoomsDataGrid.IsSynchronizedWithCurrentItem = true;
+            RoomsDataGrid.SelectedItem = null;
+
+        }
+
+        private void RoomsDataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName.ToLower() == "IsActive".ToLower())
+            {
+                e.Column.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void GuestsDataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName.ToLower() == "IsActive".ToLower())
+            {
+                e.Column.Visibility = Visibility.Collapsed;
+            }
+
+            if (e.PropertyName.ToLower() == "Id".ToLower())
+            {
+                e.Column.Visibility = Visibility.Collapsed;
+            }
+
+            if (e.PropertyName.ToLower() == "guest_reservation_id".ToLower())
+            {
+                e.Column.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void AddGuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            var addGuestWindow = new AddEditGuest();
+            Hide();
+            if (addGuestWindow.ShowDialog() == true)
+            {
+                FillData();
+            }
+            Show();
+        }
+
+        private void EditGuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            Guest chosenGuest = (Guest)GuestsDataGrid.SelectedItem;
+            if (chosenGuest == null)
+            {
+                MessageBox.Show("Please select a Guest.", "Select Guest", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            var editGuestWindow = new AddEditGuest(chosenGuest);
+            Hide();
+            if (editGuestWindow.ShowDialog() == true)
+            {
+                FillData();
+            }
+            Show();
+        }
+
+        private void DeleteGuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            var chosenGuest = (Guest)GuestsDataGrid.SelectedItem;
+            if (chosenGuest == null)
+            {
+                MessageBox.Show("Please select a Guest.", "Select Guest", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            var deleteGuestWindow = new DeleteGuest(chosenGuest);
+            Hide();
+            if (deleteGuestWindow.ShowDialog() == true)
+            {
+                FillData();
+            }
+            Show();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Room selectedRoom = RoomsDataGrid.SelectedItem as Room;
+            DateTime? startDate = StartDatePicker.SelectedDate;
+            DateTime? endDate = EndDatePicker.SelectedDate;
+
+            if (selectedRoom == null)
+            {
+                MessageBox.Show("Please select a room.");
+                return;
+            }
+
+            if (!startDate.HasValue || !endDate.HasValue)
+            {
+                MessageBox.Show("Please select both start and end dates.");
+                return;
+            }
+
+            // Not in the past
+            if (startDate < DateTime.Today)
+            {
+                MessageBox.Show("Start date cannot be in the past.");
+                return;
+            }
+
+            // Is Start date less than or equal to the end date
+            if (startDate > endDate)
+            {
+                MessageBox.Show("Start date must be equal or earlier than end date.");
+                return;
+            }
+
+            var reservations = Hotel.GetInstance().Reservations;
+            foreach (Reservation r in reservations)
+            {
+                if (selectedRoom.room_number == r.reservation_room_number && r.reservation_is_finished == false && r.reservation_is_active == true)
+                {
+                    if (AreDatesOverlapping(startDate, endDate, r.start_date_time, r.end_date_time))
+                    {
+                        MessageBox.Show("Try with different dates, these are overlapping. " + r.start_date_time + "-" + r.end_date_time);
+                        return;
+                    }
+                }
+
+            }
+
+            reservationService.SaveReservation(contextReservation, selectedRoom);
+            DialogResult = true;
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void CheckAvailableRoomsButton_Click(object sender, RoutedEventArgs e)
+        {
+            view.Refresh();
+        }
+
     }
 }
